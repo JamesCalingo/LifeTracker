01e442055a4f2754f14d7130bfa5e650078eadaa
JC <james.calingo@gmail.com>
2019-12-19 11:32:34 -0500

    continuing to work on starting HP

--- assets/js/health.js
+++ assets/js/health.js
@@ -1,12 +1,38 @@
-let p1=0
-let p2=0
-let p3=0;
-let p4=0;
+$("#multi").on("click", function(e){
+  e.preventDefault();
+  $(".main").html(`<p>Next, what is everyone's starting HP?
+  <form>
+  <div class="form-group">
+    <input type="number" class="form-control" id="HP" aria-describedby="emailHelp">
+   
+  </div>
+ 
+  <button id="HPsubmit" class="btn btn-primary">Submit</button>
+</form>`);
+});
+
+var startHP;
+$(document).on("click", "#HPsubmit", function(e){
+  e.preventDefault();
+  startHP = $("#HP").val()
+  $(".main").html(
+  `<p>And may I ask just how many players we're talking about?</p>
+  <a href="2players.html" class="button btn btn-danger">2 Players</a>
+  <a href="3players.html" class="button btn btn-success">3 Players</a>
+  <a href="4players.html" class="button btn btn-primary">4 Players</a>`
+  );
+});
+
+let p1=startHP;
+let p2=startHP;
+let p3=startHP;
+let p4=startHP;
 
 $("#hp1").html(p1);
 $("#hp2").html(p2);
 $("#hp3").html(p3);
 $("#hp4").html(p4);
+console.log(p1)
 
 function update(){
   $("#hp1").html(p1);
@@ -19,6 +45,7 @@ function update(){
   $("#hp1Up").on("click", function (event){
     event.preventDefault();
     p1++;
+    alert("HI")
     update();
   });
   
