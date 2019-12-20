bd230194f98c9a487878f6055adb7e9312d6be2e
JC <james.calingo@gmail.com>
2019-12-20 11:05:57 -0500

    have the multiplayer pages working

--- assets/js/health.js
+++ assets/js/health.js
@@ -1,27 +1,33 @@
 $("#multi").on("click", function(e){
   e.preventDefault();
-  $(".main").html(`<p>Next, what is everyone's starting HP?
-  <form>
-  <div class="form-group">
-    <input type="number" class="form-control" id="HP" aria-describedby="emailHelp">
+//   $(".main").html(`<p>Next, what is everyone's starting HP?
+//   <form>
+//   <div class="form-group">
+//     <input type="number" class="form-control" id="HP" aria-describedby="emailHelp">
    
-  </div>
+//   </div>
  
-  <button id="HPsubmit" class="btn btn-primary">Submit</button>
-</form>`);
-});
-
-var startHP = 20;
-$(document).on("click", "#HPsubmit", function(e){
-  e.preventDefault();
-  startHP = $("#HP").val()
-  $(".main").html(
+//   <button id="HPsubmit" class="btn btn-primary">Submit</button>
+// </form>`);
+$(".main").html(
   `<p>And may I ask just how many players we're talking about?</p>
   <a href="2players.html" class="button btn btn-danger">2 Players</a>
   <a href="3players.html" class="button btn btn-success">3 Players</a>
   <a href="4players.html" class="button btn btn-primary">4 Players</a>`
-  );
+)
 });
+
+var startHP = 50;
+// $(document).on("click", "#HPsubmit", function(e){
+//   e.preventDefault();
+//   startHP = $("#HP").val()
+  // $(".main").html(
+  // `<p>And may I ask just how many players we're talking about?</p>
+  // <a href="2players.html" class="button btn btn-danger">2 Players</a>
+  // <a href="3players.html" class="button btn btn-success">3 Players</a>
+  // <a href="4players.html" class="button btn btn-primary">4 Players</a>`
+//   );
+// });
 
 let p1=startHP;
 let p2=startHP;
@@ -32,7 +38,7 @@ $("#hp1").html(p1);
 $("#hp2").html(p2);
 $("#hp3").html(p3);
 $("#hp4").html(p4);
-console.log(p1)
+
 
 function update(){
   $("#hp1").html(p1);
